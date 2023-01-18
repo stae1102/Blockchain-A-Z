c1a7bbbe4ac8bece2bdec9ce999d6fe0db9e4a6e
Seongtae <stae1102@gmail.com>
2023-01-18 16:53:07 +0900

    feat: 블록체인 유효성 검사 api를 추가한다.

--- 1. Blockchain Intuition/blockchain.py
+++ 1. Blockchain Intuition/blockchain.py
@@ -104,5 +104,15 @@ def get_chain():
                'length': len(blockchain.chain) }
   return jsonify(response), 200
 
+# Checking if Block chain is valid
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+  is_valid = blockchain.is_chain_valid(blockchain.chain)
+  if is_valid:
+    response = { 'message': 'All good. The Blockchain is valid.' }
+  else:
+    response = { 'message': 'Houston, we have a problem. The Blockchain is not valid.' }
+  return jsonify(response), 200
+
 # Running the app
-app.run(host = '0.0.0.0', port = 5000)
+app.run(host = '127.0.0.1', port = 5000)
