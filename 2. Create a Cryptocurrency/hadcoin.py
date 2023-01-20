1359fd30d67ec4d222e6189fedb280d31be8a944
Seongtae <stae1102@gmail.com>
2023-01-20 15:57:00 +0900

    feat: 가장 긴 chain으로 합의하는 함수를 생성한다

--- 2. Create a Cryptocurrency/hadcoin.py
+++ 2. Create a Cryptocurrency/hadcoin.py
@@ -183,5 +183,17 @@ def connect_node():
                'total_nodes': blockchain.nodes }
   return jsonify(response), 201
 
+# Replacing the chain by the longest chain if needed
+@app.route('/replace_chain', methods = ['GET'])
+def replace_chain():
+  is_chain_replaced = blockchain.replace_chain()
+  if is_chain_replaced:
+    response = { 'message': 'The nodes had different chains so the chain was replaced by the longest',
+                 'new_chain': blockchain.chain }
+  else:
+    response = { 'message': 'All good. The chain is the largest one.',
+                 'actual_chain': blockchain.chain }
+  return jsonify(response), 200
+
 # Running the app
 app.run(host = '127.0.0.1', port = 5000)
