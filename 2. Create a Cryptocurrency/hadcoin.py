adb709c83cdfd4c0a4c01eda3e41297214a0b706
Seongtae <stae1102@gmail.com>
2023-01-20 15:11:24 +0900

    feat: node를 추가하는 함수를 추가한다

--- 2. Create a Cryptocurrency/hadcoin.py
+++ 2. Create a Cryptocurrency/hadcoin.py
@@ -22,6 +22,7 @@ class Blockchain:
     self.chain = []
     self.transactions = []
     self.create_block(proof = 1, previous_hash = '0')
+    self.nodes = set()
 
   def create_block(self, proof, previous_hash):
     block = { 'index': len(self.chain) + 1,
@@ -74,6 +75,11 @@ class Blockchain:
                                'amount': amount })
     previous_block = self.get_previous_block()
     return previous_block['index'] + 1
+
+  # address: 노드 주소
+  def add_node(self, address):
+    parsed_url = urlparse(address)
+    self.nodes.add(parsed_url.netloc)
 
 # Part 2 - Mining our Blockchains
 
