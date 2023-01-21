6c9e446bde77df75ff6ddfbb65190e28d63ec0b7
Seongtae <stae1102@gmail.com>
2023-01-21 18:17:29 +0900

    feat: coin, usd 보유 확인과 coin 구매 함수를 추가한다

--- 3. Smart Contract Intuition/hadcoin_ico.sol
+++ 3. Smart Contract Intuition/hadcoin_ico.sol
@@ -19,11 +19,30 @@ contract Hadcoin_ico {
   // Mapping from the investor address to tis equity in Hadcoins and USD
   mapping(address => uint) equity_hadcoins;
   mapping(address => uint) equity_usd;
-
+  
   // Checking if an investor can buy Hadcoins
   modifier can_buy_hadcoins(uint usd_invested) {
     require (usd_invested * usd_to_hadcoins + total_hadcoins_bought <= max_hadcoins);
     _;
+  }
+
+  // Getting the equity in Hadcoins of an investor
+  function equity_in_hadcoins(address investor) external view returns(uint){
+    return equity_hadcoins[investor];
+  }
+
+  // Getting the equity in USD of an investor
+  function equity_in_usd(address investor) external view returns(uint){
+    return equity_usd[investor];
+  }
+
+  // Buying Hadcoins
+  function buy_hadcoins(address investor, uint usd_invested) external
+  can_buy_hadcoins(usd_invested) {
+    uint hadcoins_bought = usd_invested * usd_to_hadcoins;
+    equity_hadcoins[investor] += hadcoins_bought;
+    equity_usd[investor] += equity_hadcoins[investor] / 1_000;
+    total_hadcoins_bought += hadcoins_bought;
   }
 
 }
